abe1aaa6715cab399c2bdbbd678f905785654f64
iair <dev.iair.918@gmail.com>
2023-07-11 12:33:21 +0900

    update kafka & setup env

--- dockerfile
+++ dockerfile
@@ -5,8 +5,8 @@ RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 RUN apt update && apt upgrade -y && apt install -y tzdata wget openjdk-17-jdk
 RUN mkdir /app
 WORKDIR /app
-RUN wget https://dlcdn.apache.org/kafka/3.4.0/kafka_2.13-3.4.0.tgz
-RUN tar -xzf kafka_2.13-3.4.0.tgz
-COPY ./data/properties/server.properties /app/kafka_2.13-3.4.0/config/kraft/server.properties
-COPY ./setup.sh /app/kafka_2.13-3.4.0
-COPY ./run.sh /app/kafka_2.13-3.4.0
+RUN wget https://downloads.apache.org/kafka/3.5.0/kafka_2.13-3.5.0.tgz
+RUN tar -xzf kafka_2.13-3.5.0.tgz
+COPY ./data/properties/server.properties /app/kafka_2.13-3.5.0/config/kraft/server.properties
+COPY ./setup.sh /app/kafka_2.13-3.5.0
+COPY ./run.sh /app/kafka_2.13-3.5.0
--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-rm -rf /app/data/*
-cd /app/kafka_2.13-3.4.0
-sed -i "s|KFK_NODE_ID|${KFK_NODE_ID}|g" config/kraft/server.properties
-sed -i "s|KFK_QUORUM_VOTERS|${KFK_QUORUM_VOTERS}|g" config/kraft/server.properties
-sed -i "s|KFK_ADV_LISTENERS|${KFK_ADV_LISTENERS}|g" config/kraft/server.properties
-sed -i "s|KFK_LOG_DIRS|${KFK_LOG_DIRS}|g" config/kraft/server.properties
-bin/kafka-storage.sh format -t $KFK_CLUSTER_ID -c config/kraft/server.properties
+cd /app/kafka_2.13-3.5.0
+if [ ! -f "/app/data/meta.properties" ]; then
+  sed -i "s|KFK_NODE_ID|${KFK_NODE_ID}|g" config/kraft/server.properties
+  sed -i "s|KFK_QUORUM_VOTERS|${KFK_QUORUM_VOTERS}|g" config/kraft/server.properties
+  sed -i "s|KFK_ADV_LISTENERS|${KFK_ADV_LISTENERS}|g" config/kraft/server.properties
+  sed -i "s|KFK_LOG_DIRS|${KFK_LOG_DIRS}|g" config/kraft/server.properties
+  bin/kafka-storage.sh format -t $KFK_CLUSTER_ID -c config/kraft/server.properties
+fi
 bin/kafka-server-start.sh config/kraft/server.properties
